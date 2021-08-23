c89b6de0e363616554d33dad37c1e095c71c3230
Lagishetty Sai Ram <lagishettysairam21@gmail.com>
2021-08-23 08:02:23 +0530

    my first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+  
+import React from 'react'
+
+const Header = () => {
+  return (
+      <header className="header">
+          <nav>
+              <div className="logo">
+                 {/* <img src={logo} alt="Todo-list" /> */}
+              </div>
+          </nav>
+
+      </header>
+
+  )
+}
+
+export default Header
